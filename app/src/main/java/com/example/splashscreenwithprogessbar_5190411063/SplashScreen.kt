8ddc57b5f04f501a0aef40cc8128f67f6cc712fd
firdaus <firdaus.24oktober@gmail.com>
2021-12-08 16:36:23 +0700

    First init

---
+++ app/src/main/java/com/example/splashscreenwithprogessbar_5190411063/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.splashscreenwithprogessbar_5190411063
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    private var SPLASH_SCREEN_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }, SPLASH_SCREEN_TIME)
+    }
+}
